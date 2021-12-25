2e0ef0c71f8cfe8b4452883cd21455fc1b12fbfb
Sota Hirose <za446131@gmail.com>
2021-12-25 19:31:32 +0900

    src配下に変更

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <meta name="description" content="next app template" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <p>Hello Wolrd</p>
+    </div>
+  )
+}
+
+export default Home
