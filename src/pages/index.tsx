c6c6bb9ca5df567f3a9a337142b1959591d7f66b
Sota Hirose <za446131@gmail.com>
2021-12-26 14:06:42 +0900

    都道府県別の人口数情報取得のための処理

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,49 @@ import styles from 'src/styles/Home.module.css'
 import { PrefectureInputGroup } from 'src/component/PrefectureInputGroup'
 import { useEffect, useState } from 'react'
 import { useAllPrefecture } from 'src/hooks/getAllPrefecture'
+import axios from 'axios'
+import { PrefectureInfoType } from 'src/type'
+
+const getPrefInfo = async (
+  id: number
+): Promise<{
+  message: null
+  result: PrefectureInfoType
+}> => {
+  const res = await axios.get(
+    `https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?prefCode=${
+      id + 1
+    }`,
+    {
+      headers: {
+        'X-API-KEY':
+          process.env.NEXT_PUBLIC_X_API_KEY !== undefined
+            ? process.env.NEXT_PUBLIC_X_API_KEY
+            : ''
+      }
+    }
+  )
+  const data = (await res.data) as {
+    message: null
+    result: PrefectureInfoType
+  }
+
+  return data
+}
 
 const Home: NextPage = () => {
   const { data: AllPrefectureData } = useAllPrefecture()
   const [isCheked, setIsCheked] = useState<boolean[]>([])
+  const [prefInfos, setPrefInfos] = useState<
+    Array<{
+      prefCode: number
+      data: Array<{
+        year: number
+        value: number
+      }>
+    }>
+  >([])
+
   useEffect(() => {
     setIsCheked(
       AllPrefectureData !== undefined
@@ -15,6 +54,30 @@ const Home: NextPage = () => {
         : []
     )
   }, [AllPrefectureData])
+
+  useEffect(() => {
+    const changeState = async (): Promise<void> => {
+      const results: Array<{
+        prefCode: number
+        data: Array<{
+          year: number
+          value: number
+        }>
+      }> = []
+      for (let i = 0; i < isCheked.length; i++) {
+        if (isCheked[i]) {
+          const data = await getPrefInfo(i)
+          const result = {
+            prefCode: i + 1,
+            data: data.result.data[0].data
+          }
+          results.push(result)
+        }
+      }
+      setPrefInfos(results)
+    }
+    void changeState()
+  }, [isCheked])
 
   if (AllPrefectureData === undefined) return <div>loading...</div>
   return (
--- src/type.ts
+++ src/type.ts
@@ -2,3 +2,8 @@ export interface AllPrefectureType {
   message: null
   result: Array<{ prefCode: number; prefName: string }>
 }
+
+export interface PrefectureInfoType {
+  boundaryYear: number
+  data: Array<{ label: string; data: Array<{ year: number; value: number }> }>
+}
